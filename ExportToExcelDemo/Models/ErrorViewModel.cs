a35126421d42ac395734eb1184e3b2f53829abdb
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-03 19:03:22 +0500

    Initial commit.

---
+++ ExportToExcelDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExportToExcelDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
